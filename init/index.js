576f19f040f9afebf887dc9f92b6c0c6a53585e3
Raghav Somani <raghavsomani102003@gmail.com>
2026-03-17 21:57:12 +0530

    commit after changes

--- init/index.js
+++ init/index.js
@@ -1,10 +1,18 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config()    
+}
+
 // Initialization Logic
 
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/travellister";
+// local system mongoDB database
+// const MONGO_URL = "mongodb://127.0.0.1:27017/travellister";
+
+// cloud or Mongo Atlas mongoDB database 
+const dbURL = process.env.ATLASDB_URL; 
 
 main().then(() => {
     console.log("connected to DB");
@@ -13,7 +21,8 @@ main().then(() => {
 })
 
 async function main(){
-    mongoose.connect(MONGO_URL);
+    // mongoose.connect(MONGO_URL); // mongoose connects with local system mongoDB database
+    mongoose.connect(dbURL);  // mongoose connects with cloud mongoDB database
 }
 
 const initDB = async () => {
@@ -21,7 +30,7 @@ const initDB = async () => {
 
     // Adding Owner Property in all Listings
     // using spread so that, all remaining fields remain as it is as before; just add a new field owner to all listings
-    initData.data = initData.data.map((obj) => ({...obj, owner: "69b5221fdcc4e6ff4f1bdd85"}));
+    initData.data = initData.data.map((obj) => ({...obj, owner: "69b95b68b8a17f8ea94314c9"}));
 
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
